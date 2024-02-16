6391de10f7cb149351dbe112fcc89f3a1233bc1a
amhambi95@gmail.com <Asiphe Mhambi>
2024-02-16 14:48:15 +0200

    installing style component

--- src/components/Home/MainSection.js
+++ src/components/Home/MainSection.js
@@ -1,11 +1,21 @@
-// src/Main.js
 import React from 'react';
 import image from './images/abt.jpg';
 import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
+import styled from 'styled-components';
+
+const Section = styled.section`
+  /* Your section styles */
+  @media screen and (max-width: 768px) {
+    /* Adjust styles for smaller screens */
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+`;
 
 const MainSection = () => {
   return (
-    <section className="main" id="main">
+    <Section className="main" id="main">
       <div className="content">
         <h2>
           Hello, I am
@@ -22,24 +32,23 @@ const MainSection = () => {
         </div>
       </div>
       <div className="media-icons">
-          <div className='facebook'>
+        <div className='facebook'>
           <a href="https://www.facebook.com/profile.php?id=100092504071844"><FaFacebook style={{ color: '#3b5998', fontSize: '40px' }}/></a>
-          </div>
-          <div className='github'><a href="https://github.com/AsipheMhambi?tab=repositories"><FaGithub style={{ color: '#24292e', fontSize: '40px' }} /></a>
-          </div>
-          <div className='linkedin'><a href='www.linkedin.com/in/asiphe-mhambi-81504525a'><FaLinkedin  style={{color: '#3b5998', fontSize: '40px'}}/></a>
-
-          </div>
         </div>
+        <div className='github'><a href="https://github.com/AsipheMhambi?tab=repositories"><FaGithub style={{ color: '#24292e', fontSize: '40px' }} /></a>
+        </div>
+        <div className='linkedin'><a href='www.linkedin.com/in/asiphe-mhambi-81504525a'><FaLinkedin  style={{color: '#3b5998', fontSize: '40px'}}/></a>
+        </div>
+      </div>
       <div className='abtImageContainer'>
-      <div className="abtImage">
-        <img src={image} alt="Asiphe Mhambi" />
-        <div className="hire">
-                <a href="#">Hire me</a>
-            </div>
+        <div className="abtImage">
+          <img src={image} alt="Asiphe Mhambi" />
+          <div className="hire">
+            <a href="#">Hire me</a>
+          </div>
         </div>
       </div> 
-    </section>
+    </Section>
   );
 };
 
--- src/components/Projects/ProjectsSection.js
+++ src/components/Projects/ProjectsSection.js
@@ -3,11 +3,13 @@ import './Projects.css';
 
 
 
+
+
 const ProjectsSection = () => {
   const projects = [
     { image: ('images/game.jpg'), type: 'Game', name: 'Game Project' },
-    { image: ('images/financial.jpg'), type: 'Financial Calculator', name: 'Financial Calculator Project' },
-    { image: ('../Projects/tax.png'), type: 'Tax Calculator', name: 'Tax Calculator Project' },
+    { image: ('images/financial.png'), type: 'Financial Calculator', name: 'Financial Calculator Project' },
+    { image: ('./tax.jpg'), type: 'Tax Calculator', name: 'Tax Calculator Project' },
     { image: ('images/company.jpg'), type: 'Company Website', name: 'Company Website Project' },
     { image: ('images/chat.jpg'), type: 'Chat App', name: 'Chat App Project' },
     { image: ('images/bank.jpg'), type: 'Bank App', name: 'Bank App Project' }
@@ -18,7 +20,7 @@ const ProjectsSection = () => {
       <div className="card-list">
         {projects.map((project, index) => (
           <a href="#" className="card-item" key={index}>
-            <img src={project.image.default} alt={`${project.type} Images`} />
+            <img src={project.image.default} alt={`${project.type} images`} />
             <span className="project-type">{project.type}</span>
             <h3>{project.name}</h3>
             <div className="arrow">
