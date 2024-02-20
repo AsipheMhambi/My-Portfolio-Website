ec884b9e80e875e046e089ddbe36867c809398d8
amhambi95@gmail.com <Asiphe Mhambi>
2024-02-20 14:30:56 +0200

    edited my project page content,cards and hovering

--- src/components/Projects/ProjectsSection.js
+++ src/components/Projects/ProjectsSection.js
@@ -1,35 +1,51 @@
-import React from 'react';
-import './Projects.css';
-import Tax from './Images/tax.png'
-import game from './Images/game.jpg'
-import Web from './Images/website.png'
-import Converter from './Images/converter.png'
-import {FaGithub} from 'react-icons/fa'
-
-
-
+import React from "react";
+import "./Projects.css";
+import project1 from './Images/website.png';
+import project2 from './Images/converter.png';
+import project3 from './Images/game.jpg';
+import project4 from './Images/tax.png';
 
 const ProjectsSection = () => {
-  const projects = [
-    { image: game, type: 'Game' },
-    { image: Converter, type: 'Unit Converter'},
-    { image: Web, type: 'Company Website' },
-    { image: Tax, type: 'Tax Calculator' }
-  ];
-
   return (
-    <section className="projects" id="projects">
-      <div className="card-list">
-        {projects.map((project, index) => (
-          <a href="#" className="card-item" key={index}>
-            <img src={project.image} alt={`${project.type} images`} />
-            <span className="project-type">{project.type}</span>
-            <h3>{project.name}</h3>
-            <div className="arrow">
-              <i className='github'><a href="https://github.com/AsipheMhambi?tab=repositories"><FaGithub style={{ color: '#24292e', fontSize: '40px' }} /></a></i>
+    <section id="project">
+      <div className="project_container">
+        <h2 className="projecttitle">My Projects</h2>
+        <span className="projectDec">What I did to gain experience</span>
+        <div className="projectImgs">
+          <div className="projectItem">
+            <img src={project1} alt="" className="projectImg" />
+            <div className="hoverText">
+              <div className="projectText">
+                This is a company website whereby we showcase ourselves and capabilities.
+              </div>
             </div>
-          </a>
-        ))}
+          </div>
+          <div className="projectItem">
+          <img src={project2} alt="" className="projectImg" />
+            <div className="hoverText">
+              <div className="projectText">
+                This is a unit converter that is made for several conversions.
+              </div>
+            </div>
+          </div>
+          <div className="projectItem">
+          <img src={project3} alt="" className="projectImg" />
+            <div className="hoverText">
+              <div className="projectText">
+              This a snake game that you can enjoy playing it even if you dont like games but you can enjoy this one.
+              </div>
+            </div>
+          </div>
+          <div className="projectItem">
+            <img src={project4} alt="" className="projectImg" />
+            <div className="hoverText">
+              <div className="projectText">
+              This is a calculator for investment or bonds, where a user can
+                input principal, interest rate, and time.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
