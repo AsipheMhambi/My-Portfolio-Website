3f137618335582bb10d0c1cf24182aeb63b6c66b
amhambi95@gmail.com <Asiphe Mhambi>
2024-02-20 10:51:11 +0200

    added images for projects page

--- src/components/Projects/ProjectsSection.js
+++ src/components/Projects/ProjectsSection.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Projects.css';
-
+import Tax from './Images/tax.png'
+import game from './Images/game.jpg'
+import Web from './Images/website.png'
+import Converter from './Images/converter.png'
+import {FaGithub} from 'react-icons/fa'
 
 
 
 
 const ProjectsSection = () => {
   const projects = [
-    { image: ('images/game.jpg'), type: 'Game', name: 'Game Project' },
-    { image: ('images/financial.png'), type: 'Financial Calculator', name: 'Financial Calculator Project' },
-    { image: ('./tax.jpg'), type: 'Tax Calculator', name: 'Tax Calculator Project' },
-    { image: ('images/company.jpg'), type: 'Company Website', name: 'Company Website Project' },
-    { image: ('images/chat.jpg'), type: 'Chat App', name: 'Chat App Project' },
-    { image: ('images/bank.jpg'), type: 'Bank App', name: 'Bank App Project' }
+    { image: game, type: 'Game' },
+    { image: Converter, type: 'Unit Converter'},
+    { image: Web, type: 'Company Website' },
+    { image: Tax, type: 'Tax Calculator' }
   ];
 
   return (
@@ -20,11 +22,11 @@ const ProjectsSection = () => {
       <div className="card-list">
         {projects.map((project, index) => (
           <a href="#" className="card-item" key={index}>
-            <img src={project.image.default} alt={`${project.type} images`} />
+            <img src={project.image} alt={`${project.type} images`} />
             <span className="project-type">{project.type}</span>
             <h3>{project.name}</h3>
             <div className="arrow">
-              <i className="fas fa-arrow-right card-icon"></i>
+              <i className='github'><a href="https://github.com/AsipheMhambi?tab=repositories"><FaGithub style={{ color: '#24292e', fontSize: '40px' }} /></a></i>
             </div>
           </a>
         ))}
