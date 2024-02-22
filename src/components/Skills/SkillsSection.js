261b009f9db1e2521537926d6cf86c13de076134
amhambi95@gmail.com <Asiphe Mhambi>
2024-02-22 10:27:24 +0200

    added text in my skills page

--- src/components/Skills/SkillsSection.js
+++ src/components/Skills/SkillsSection.js
@@ -17,7 +17,10 @@ const SkillsSection = () => {
           <div className="skillItem">
             <img src={skill1Image} alt="" className="skillImg" />
             <div className="hoverText">
-              <div className="skillText">          
+              <div className="skillText">
+              My expertise in HTML and CSS enables me to create responsive, 
+              visually appealing websites, 
+              blending content structuring with creative design for seamless user experiences.          
               </div>
             </div>
           </div>
@@ -25,7 +28,9 @@ const SkillsSection = () => {
           <div className="skillItem">
           <img src={skill3Image} alt="" className="skillImg" />
             <div className="hoverText">
-              <div className="skillText">   
+              <div className="skillText">
+              My proficiency in JavaScript enhances my web development skills, 
+              allowing me to build interactive, efficient web applications that provide dynamic user experiences.   
               </div>
             </div>
           </div>
@@ -33,7 +38,9 @@ const SkillsSection = () => {
           <div className="skillItem">
           <img src={skill4Image} alt="" className="skillImg" />
             <div className="hoverText">
-              <div className="skillText">              
+              <div className="skillText">
+              Skilled in React, I excel at developing highly interactive and stateful web applications, 
+              leveraging component-based architecture for reusable and efficient UIs              
               </div>
             </div>
           </div>
@@ -41,7 +48,9 @@ const SkillsSection = () => {
           <div className="skillItem">
           <img src={skill5Image} alt="" className="skillImg" />
             <div className="hoverText">
-              <div className="skillText">              
+              <div className="skillText">
+              Proficient in Python, I leverage its simplicity and versatility to develop robust applications, 
+              automate tasks, and analyze data with efficiency and ease.              
               </div>
             </div>
           </div>
@@ -50,6 +59,8 @@ const SkillsSection = () => {
           <img src={skill6Image} alt="" className="skillImg" />
             <div className="hoverText">
               <div className="skillText">
+              Experienced in Java, I utilize its strong object-oriented principles to develop scalable and reliable applications, 
+              ensuring high performance and maintainability.
               </div>
             </div>
           </div>
@@ -58,6 +69,8 @@ const SkillsSection = () => {
             <img src={skill7Image} alt="" className="skillImg" />
             <div className="hoverText">
               <div className="skillText">
+              Skilled in Node.js, I harness the power of JavaScript on the server side to build fast, 
+              scalable network applications, excelling in real-time data processing and APIs.
               </div>
             </div>
           </div>
